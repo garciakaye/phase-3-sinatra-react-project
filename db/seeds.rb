b395632980e3b8a8795290470d74dba716450f6b
kaye garcia <kayemgarcia@gmail.com>
2021-12-06 16:20:41 -0600

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,8 @@ List.create(title: "Home")
 List.create(title: "School")
 List.create(title: "Work")
 
+Item.create(name: "laundry", list_id: 1)
+Item.create(name: "set up React file for project", list_id: 2)
+Item.create(name: "schedule zoom meeting for Edward", list_id: 3)
+
 puts "✅ Done seeding!"
